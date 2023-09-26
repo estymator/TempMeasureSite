39bb1db55b0ea22bbf4d8db4fcaa69ff93a9b2eb
estymator <pawel.chelmecki55@gmail.com>
2023-09-26 21:11:04 +0200

    import vue-chart-js lib

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
 </script>
 
 <template>
     Chart
+    <Bar
+    id="testBar"/>
 </template>
